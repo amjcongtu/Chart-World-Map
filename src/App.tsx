7028e2546c8ae2c9d6e59afcff1ac3fcc9e6c373
Amj <hoangxuan111093@gmail.com>
2024-02-15 18:09:05 +0700

    update react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
+// import { QueryClient, QueryClientProvider } from "react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import { lazy } from "react";
 import { Layout } from "./components/Layout";
 import "./App.css";
--- src/services/queries/useCountries.ts
+++ src/services/queries/useCountries.ts
@@ -1,22 +1,17 @@
-import { useQuery } from "react-query";
 import { getAPI } from "../apiClient";
 import { get } from "lodash";
 import { queryKeys } from "../../utils/constant/query";
+import { useQuery } from "@tanstack/react-query";
 
 export const API_COUNTRIES = "/v3.1/all";
 
 export const useCountries = () => {
-  const url = `${
-    import.meta.env.VITE_APP_BASE_API_URL
-  }${API_COUNTRIES}`;
+  const url = `${import.meta.env.VITE_APP_BASE_API_URL}${API_COUNTRIES}`;
 
   const fetcher = async () => {
     const result = await getAPI(url);
-    return get(result, "data", []) ;
+    return get(result, "data", []);
   };
 
-  return useQuery([queryKeys.countries], fetcher, {
-    // cache upload data
-    // cacheTime: 100,
-  });
+  return useQuery({ queryKey: [queryKeys.countries], queryFn: fetcher });
 };
